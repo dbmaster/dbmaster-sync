b2c7aca917fb094abfd7b2cc1cea234d9ef6de41
Slava Christin <slava@branegy.com>
2016-03-15 14:18:37 -0400

    Add show changes only parameter to details; unhide session_id

--- src/main/resources/sync-history-details.groovy
+++ src/main/resources/sync-history-details.groovy
@@ -1,7 +1,7 @@
 import com.branegy.dbmaster.sync.api.*
 import io.dbmaster.sync.*
 
-logger.info("Load session")
+logger.info("Loading session ${p_session_id}")
 
 SyncService syncService = dbm.getService(SyncService.class)
 
@@ -13,7 +13,7 @@ println "<h1>Synchronization session: ${p_title}</h1>"
 
 if (session.getParameter("html")==null) {
     session = syncService.findSessionById(p_session_id, true)
-    println syncService.generateSyncSessionPreviewHtml(session, false)
+    println syncService.generateSyncSessionPreviewHtml(session, p_show_changes_only)
 } else {
     println session.getParameter("html")
     logger.info("Use html value")
